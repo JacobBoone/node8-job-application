9dfcf6c01e5440666ea02f937caf737128d3a595
Jacob <jakeboone@aol.com>
2014-07-29 15:53:40 -0600

    TMNT, completed section 4

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Candidate =require('./models/candidates.js')
 
 mongoose.connect('mongodb://localhost/omega3studios');
 
@@ -9,7 +10,7 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
 	res.render('index');
@@ -17,15 +18,33 @@ app.get('/', function(req, res) {
 
 // displays a list of applicants
 app.get('/applicants', function(req, res){
-	res.render('applicants')
+	Candidate.find({}, function(err, data){
+		res.render('applicants', {applicants: data})
+
+	})
+	
 });
+
 // creates and applicant
 app.post('/applicant', function(req, res){
 	console.log(req.body)
+
+	var candidate = new Candidate({
+		name: req.body.name,
+		bio: req.body.bio, 
+		skills: req.body.skills,
+		years: req.body.years,
+		why: req.body.why
+	});
+
+	candidate.save()
+
 	// Here is where you need to get the data
 	// from the post body and store it in the database
 	res.render('./submitted');
-});
+	// res.render('candidate', );
+	});
+
 
 var server = app.listen(8441, function() {
 	console.log('Express server listening on port ' + server.address().port);
